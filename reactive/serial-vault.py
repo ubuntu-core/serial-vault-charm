f97c0395c51d98becfb37919cd9631cca96e38cd
James Jesudason <james.jesudason@gmail.com>
2016-06-24 15:06:11 +0100

    Deploy the service using a snap from the store.

--- reactive/serial-vault.py
+++ reactive/serial-vault.py
@@ -1,6 +1,6 @@
 import os
 import logging
-from subprocess import call
+from subprocess import call, check_output, PIPE, Popen
 
 from charms.reactive import when, hook
 from charms.reactive import is_state, set_state, remove_state
@@ -8,22 +8,29 @@ from charmhelpers import fetch
 from charmhelpers.core import hookenv
 from charmhelpers.core import templating
 from charmhelpers.core.hookenv import (
-    config, relation_set, relation_get,
-    local_unit, related_units, remote_unit)
+    config, local_unit, log, relation_get, related_units)
 
 
-logger = logging.getLogger('serial-vault')
+PORTS = {
+    'admin': 8081,
+    'signing': 8080
+}
 
 
 @hook('install')
 def install():
+    """
+    Fetches the Serial Vault snap and installs it. Configuration cannot
+    be done until the database is available.
+    """
     if is_state('serial-vault.available'):
         return
-    logger.info("***install_serial_vault")
-    print("***install_serial_vault")
 
-    # Install the dependency packages
-    install_packages()
+    # Open the relevant port for the service
+    open_port()
+
+    # Install the sanap, but it won't be ready until it has a db connection
+    install_snap()
 
     hookenv.status_set('maintenance', 'Waiting for database')
     set_state('serial-vault.available')
@@ -31,88 +38,89 @@ def install():
 
 @hook('config-changed')
 def config_changed():
-    logger.info("***config_changed_serial_vault")
-    print("***config_changed_serial_vault")
+    configure_service()
 
 
-@hook(
-    'database-relation-changed')
+@hook('database-relation-changed')
 def db_relation_changed(*args):
-    logger.info("***DB RELATION CHANGED - setup_serial_vault")
-    print("***DB RELATION CHANGED - setup_serial_vault")
+    configure_service()
 
-    database = {}
 
-    for db_unit in related_units():
-        # if relation_get('database', db_unit) != config('database'):
-        #     continue  # Not yet acknowledged requested database name.
+def configure_service():
+    """
+    Get the database settings and create the service config file. Pipe it to
+    the service using the config command. This will overwrite the settings on
+    the snap's filesystem.
+    """
+    hookenv.status_set('maintenance', 'Configure the service')
 
-        remote_state = relation_get('state', db_unit)
-        if remote_state in ('master', 'standalone'):
-            database = relation_get(unit=db_unit)
+    database = get_database()
+    if not database:
+        return
 
-    # Configure and install the snap when the database is ready
-    install_snap(database)
+    # Create the configuration file for the snap
+    create_settings(database)
+
+    # Send the configuration file to the snap
+    ps = Popen(['cat', 'settings.yaml'], stdout=PIPE)
+    check_output([
+        'sudo', '/snap/bin/serial-vault.config'], stdin=ps.stdout)
+    ps.wait()
+
+    # Restart the snap
+    call([
+        'sudo', 'systemctl', 'restart',
+        'snap.serial-vault.serial-vault.service'])
 
     hookenv.status_set('active', '')
     set_state('serial-vault.active')
 
 
-def install_packages():
-    config = hookenv.config()
-    hookenv.status_set('maintenance', 'Installing packages')
-    packages = ['snapcraft']
-    fetch.apt_install(fetch.filter_installed_packages(packages))
-    logger.info("***install packages done")
-    print("***install packages done")
+def get_database():
+
+    if not relation_get('database'):
+        log("Database not ready yet... skipping it for now")
+        hookenv.status_set('maintenance', 'Waiting for database')
+        return None
+
+    database = None
+    for db_unit in related_units():
+        remote_state = relation_get('state', db_unit)
+        if remote_state in ('master', 'standalone'):
+            database = relation_get(unit=db_unit)
+
+    if not database:
+        log("Database not ready yet... skipping it for now")
+        hookenv.status_set('maintenance', 'Waiting for database')
+        return None
+
+    return database
 
 
-def install_snap(postgres):
+def install_snap():
     hookenv.status_set('maintenance', 'Install snap')
-    logger.info('*** INSTALL SNAP')
-    print('*** INSTALL SNAP')
-    logger.info(postgres)
-    print(postgres)
 
-    # Configure the snapcraft file to deploy the requested service
-    configure_snapcraft()
-
-    # Fetch the snap assets using snapcraft
-    call(['snapcraft', 'pull'])
-
-    # Override the settings.yaml file in the parts directory
-    configure_service(postgres)
-
-    # Build the snap using snapcraft
-    call(['snapcraft'])
-
-    # Install the snap
-    call(['sudo', 'snap', 'install', 'serial-vault_0.1_amd64.snap'])
+    # Fetch the snap from the store and install it
+    call(['sudo', 'snap', 'install', 'serial-vault'])
 
     hookenv.status_set('maintenance', 'Installed snap')
 
 
-def configure_snapcraft():
-    hookenv.status_set('maintenance', 'Configuring snapcraft')
-    config = hookenv.config()
-    templating.render(
-        source='snapcraft.yaml',
-        target='snapcraft.yaml',
-        context={
-            'service_type': config['service_type'],
-        }
-    )
-    hookenv.status_set('maintenance', '')
-
-
-def configure_service(postgres):
+def create_settings(postgres):
     hookenv.status_set('maintenance', 'Configuring service')
     config = hookenv.config()
     templating.render(
         source='settings.yaml',
-        target='parts/assets/src/settings.yaml',
+        target='settings.yaml',
         context={
             'keystore_secret': config['keystore_secret'],
+            'service_type': config['service_type'],
             'db': postgres,
         }
     )
+
+
+def open_port():
+    config = hookenv.config()
+    if PORTS.get(config['service_type']):
+        hookenv.open_port(PORTS[config['service_type']], protocol='TCP')
